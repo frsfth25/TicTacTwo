ad75c9a4b9ba446a2ca1aa03f9af577c2b87e980
Faris Fathurrahman <farisfathurrahman.25@gmail.com>
2018-06-02 19:18:19 +0300

    Second commit: - ElementAdapter class - element_item layout

---
+++ app/src/main/java/com/example/farisfathurrahman25/tictactwo/ElementAdapter.java
@@ -0,0 +1,45 @@
+package com.example.farisfathurrahman25.tictactwo;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import java.util.List;
+
+public class ElementAdapter extends ArrayAdapter<Element> {
+    /**
+     * Constructor
+     *
+     * @param context  The current context.
+     * @param resource The resource ID for a layout file containing a TextView to use when
+     *                 instantiating views.
+     * @param objects  The objects to represent in the ListView.
+     */
+    public ElementAdapter(@NonNull Context context, int resource, @NonNull List<Element> objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View v = convertView;
+
+        if (null == v) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(R.layout.element_item, null);
+        }
+
+        Element barrier = getItem(position);
+
+        ImageView img = v.findViewById(R.id.elementView);
+
+        img.setImageResource(barrier.getImageId());
+
+        return v;
+    }
+}
