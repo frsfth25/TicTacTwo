9a30798808ab7df33f5dc21067db605ed49b2aa4
Faris Fathurrahman <farisfathurrahman.25@gmail.com>
2018-06-03 00:34:39 +0300

    Fourth commit: - random movement opponent

--- app/src/main/java/com/example/farisfathurrahman25/tictactwo/MainActivity.java
+++ app/src/main/java/com/example/farisfathurrahman25/tictactwo/MainActivity.java
@@ -4,14 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     //String TAG = "";
+
+    static int playerTurn = 1;
 
     static int[][] gridField =
             {
@@ -19,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
                     {0, 0, 0},
                     {0, 0, 0}
             };
-    static int elementMode = 0;
+    Button buttonClick;
     GridView gridView;
     ArrayList gridItems;
     private List<Element> elementList;
@@ -29,19 +33,34 @@ public class MainActivity extends AppCompatActivity {
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             StringBuilder sb = new StringBuilder();
             sb.append("x: ").append(position % 3).append(", y: ").append(position / 3);
-            Toast.makeText(getApplicationContext(), sb.toString(), Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getApplicationContext(), sb.toString(), Toast.LENGTH_SHORT).show();
+
+            if ((int) gridItems.get(position) == 1 || (int) gridItems.get(position) == 2) {
+                Toast.makeText(getApplicationContext(), "Already filled!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Random rand = new Random();
+            boolean found = false;
+            int filledCounter = 0;
+            while (!found || filledCounter == gridItems.size()) {
+                int idx = rand.nextInt(((gridItems.size() - 1 - 0) + 1) + 0);
+                if ((int) gridItems.get(idx) != 0) {
+                    filledCounter++;
+                    continue;
+                }
+                //int row = rand.nextInt(((2) + 1));
+                //int col = rand.nextInt(((2) + 1));
+                gridItems.set(idx, 1);
+                found = true;
+            }
+
 
             elementList = new ArrayList<>();
 
             switch ((int) gridItems.get(position)) {
-                case 1:
-                    gridItems.set(position, 2);
-                    break;
-                case 2:
-                    gridItems.set(position, 0);
-                    break;
                 default:
-                    gridItems.set(position, 1);
+                    gridItems.set(position, 2);
                     break;
             }
 
@@ -62,6 +81,14 @@ public class MainActivity extends AppCompatActivity {
 
             elementAdapter = new ElementAdapter(MainActivity.this, R.layout.element_item, elementList);
             gridView.setAdapter(elementAdapter);
+
+            if (checkGameStatus())
+                return;
+        }
+
+        public boolean checkGameStatus() {
+            //
+            return false;
         }
     };
 
@@ -69,6 +96,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonClick = findViewById(R.id.testBtn);
 
         gridView = findViewById(R.id.sceneGrid);
 
@@ -105,5 +134,11 @@ public class MainActivity extends AppCompatActivity {
 
         //register scene item click
         gridView.setOnItemClickListener(onGridClick);
+    }
+
+    public void onClickBtn(View view) {
+        Random rand = new Random();
+        int num = rand.nextInt(((2) + 1));
+        Toast.makeText(this, "" + num, Toast.LENGTH_SHORT).show();
     }
 }
